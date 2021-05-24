3db625c2b1a10faa455ddc133b0d09faa0901c0b
tsukushibito <tsukushibito@gmail.com>
2021-05-24 17:21:01 +0900

    Add task to app and install material ui to web

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   get 'users/new'
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :v1 do
+    resources :tasks
     mount_devise_token_auth_for 'User', at: 'auth'
   end
 end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_110729) do
+ActiveRecord::Schema.define(version: 2021_05_24_080603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
