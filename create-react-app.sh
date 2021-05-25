7944c23018504f1d81a076d48ad172ecb6a4c5dd
tsukushibito <tsukushibito@gmail.com>
2021-05-25 16:53:47 +0900

    Change react template javascript to typescript

--- create-react-app.sh
+++ create-react-app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . .env
-docker-compose run --rm web sh -c "npm install -g create-react-app && create-react-app ."
+docker-compose run --rm web sh -c "npm install -g create-react-app && create-react-app . --template typescript"
