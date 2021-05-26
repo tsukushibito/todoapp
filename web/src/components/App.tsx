7ae60ed7dc61b2e244fc5dbb0aaa8363b0c27ea4
tsukushibito <tsukushibito@gmail.com>
2021-05-26 19:02:11 +0900

    Make temporary TodoList

---
+++ web/src/components/App.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { TodoList } from './TodoList';
+
+export const ReactLogo = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+      </a>
+      </header>
+    </div>
+  );
+};
+
+function App() {
+  // return (
+  //   <RecoilRoot>
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <p>
+  //           Edit <code>src/App.tsx</code> and save to reload.
+  //         </p>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Learn React
+  //         </a>
+  //       </header>
+  //     </div>
+  //   </RecoilRoot>
+  // );
+  return (
+    <RecoilRoot>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ReactLogo} />
+          <Route exact path="/todolist" component={TodoList} />
+        </Switch>
+      </BrowserRouter>
+    </RecoilRoot>
+  );
+}
+
+export default App;
---
+++ web/src/components/TodoList.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { useRecoilValue } from "recoil";
+import { todoListState } from "../recoil/atoms";
+import { TodoItemCreator } from "./TodoItemCreator";
+import { TodoListItem } from "./TodoListItem";
+
+export const TodoList = () => {
+    const todoList = useRecoilValue(todoListState);
+
+    return (
+        <>
+            {/* <TodoListStats /> */}
+            {/* <TodoListFilters /> */}
+            <TodoItemCreator />
+
+            {todoList.map((todoItem) => (
+                <TodoListItem key={todoItem.id} item={todoItem} />
+            ))}
+        </>
+    );
+};
