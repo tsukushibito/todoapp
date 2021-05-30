8a285715460a3989bf5de605c652b26396877537
tsukushibito <tsukushibito@gmail.com>
2021-05-30 09:52:10 +0900

    Remove unused

--- web/src/components/pages/Home.tsx
+++ web/src/components/pages/Home.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
 import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
 import Container from '@material-ui/core/Container';
 import { TodoList } from '../TodoList';
 
